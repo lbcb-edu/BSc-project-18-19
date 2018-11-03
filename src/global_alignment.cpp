07c155fb2a35d8ad7c465575995f48a868b343ba
lbcb-edu <josipa.lipovac@fer.hr>
2018-11-03 17:40:04 +0100

    Algorithms for alignment

---
+++ src/global_alignment.cpp
@@ -0,0 +1,53 @@
+#include <isotream>
+#include <string>
+#define MATCH 0
+#define INSERT 1
+#define DELETE 2
+#include "pink_alingment.hpp"
+
+
+int needleman_wunsch(const char* query, int rows,
+                     const char* target, int cols,
+                     cell** m,
+                     int match,
+                     int mismatch,
+                     int gap){
+    
+    int i, j;
+    int opt[3];
+    std::string s;
+    std::string t;
+    
+    s {std::string (query, rows)};
+    t {std::string (target, cols)};
+    
+    for (i = 0; i <= rows; i++){
+        for (j = 0; j <= cols; j++){
+            pink::cell_init(m, i, j);
+        }
+    }
+    
+    for (i = 1; i < rows; ++i) {
+        for (j = 1; j < cols; ++j) {
+            
+            opt[MATCH] = m[i - 1][j - 1].cost + pink::match(query[i-1], target[j-1], match, mismatch);
+            opt[INSERT] = m[i][j - 1].cost + pink::indel(t[j], gap);
+            opt[DELETE] = m[i - 1][j].cost + pink::indel(s[i], gap);
+           
+            m[i][j].cost = 0;
+            
+            for (k = MATCH; k <= DELETE; k++){
+                
+                if (opt[k] > m[i][j].cost){
+                    m[i][j].cost = opt[k];
+                    m[i][j].parent = k;
+                
+                }
+            }
+        }
+    }
+    
+    pink::goal_cell(s, t, &i, &j);
+    
+    return m[i][j].cost;
+}
---
+++ src/pink_alignment.hpp
@@ -0,0 +1,60 @@
+
+namespace pink {
+    
+    enum AlignmentType {global, local, semi_global};
+    
+    typedef struct {
+        int cost;
+        int parent;
+    } cell;
+    
+    void cell_init(cell** m, int i, int j);
+    void matrix_init(int num_rows, int num_cols;
+    int match(char c, char d, int cost_pos, int cost_neg);
+    int indel(char c, int cost);
+    void goal_cell(char *s, char *t, int *x, int *y)
+    void global_alignment (const char* query, int rows,
+                           const char* target, int cols,
+                           cell** m,
+                           int match,
+                           int mismatch,
+                           int gap);
+    
+}
+
+void cell_init(cell** m, int i, int j){
+    m[i][j].cost = 0;
+    m[i][j].parent = -1;
+}
+cell** matrix_init(int num_rows, int num_cols){
+    cell m [num_rows][num_cols];
+    m[0][0] = 0;
+    return m;
+}
+
+int match(char c, char d, int cost_pos, int cost_neg){
+    if (c == d)
+        return cost_pos;
+    else
+        return cost_neg;
+}
+
+int indel(char c, int cost){
+    return cost;
+}
+
+void goal_cell(char *s, char *t, int *x, int *y){
+    int i, j;
+    *x = *y = 0;
+    for (i = 0; i < strlen(s); i++){
+        for(j = 0; j < stlen(t); j++){
+            if (m[i][j].cost > m[*x][*y].cost){
+                *x = i;
+                *y = j;
+            }
+        }
+    }
+}
+
+
+
