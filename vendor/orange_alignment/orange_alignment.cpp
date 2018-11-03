fc4b1cab1fd96993033424e03e00898fcad71396
RJPenic <rafael-josip.penic@fer.hr>
2018-11-03 15:19:28 +0100

    Functions initMatrix and populateMatrix now receive type of alignment as an argument. Function populateMatrix now returns alignmentScore. Added semi-global and local alignment functionality.

--- vendor/orange_alignment/orange_alignment.cpp
+++ vendor/orange_alignment/orange_alignment.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string>
+#include <string.h>
 #include <vector>
 
 #define MATCH 0
@@ -8,6 +9,8 @@
 
 namespace orange {
 
+    enum class AlignmentType {global, local, semi_global};
+
     namespace {
         
         typedef struct {
@@ -15,40 +18,72 @@ namespace orange {
             int parent;
         } cell;
 
-        void initMatrix (std::vector<std::vector<cell> > &matrix, int query_length, int target_length, int cost) {
+        void initMatrix (std::vector<std::vector<cell> > &matrix, int query_length, int target_length, int cost, AlignmentType type) {
+            int startRowMultiplyFactor, startColumnMultiplyFactor;
+	    switch(type) {
+		case AlignmentType::global :
+			startRowMultiplyFactor = startColumnMultiplyFactor = cost;
+			break;		
+		case AlignmentType::local :
+			startRowMultiplyFactor = startColumnMultiplyFactor = 0;
+			break;
+		case AlignmentType::semi_global :
+			startRowMultiplyFactor = 0;
+			startColumnMultiplyFactor = cost;
+	    }
+
+
+	    matrix[0][0].cost = 0;
+
             int i;
-            for (i=0; i<=target_length; i++){
-                matrix[0][i].cost = i*cost;
+            for (i=1; i<=target_length; i++){
+                matrix[0][i].cost = i*startRowMultiplyFactor;
+		matrix[0][i].parent = INSERT;
             }
-            for (i=0; i<=query_length; i++){
-                matrix[i][0].cost = i*cost;
+            for (i=1; i<=query_length; i++){
+                matrix[i][0].cost = i*startColumnMultiplyFactor;
+		matrix[i][0].parent = DELETE;
             }
         }
 
-        void populateMatrix (std::vector<std::vector<cell> > &matrix, const char* query, const char* target, int match, int mismatch, int gap){
+        int populateMatrix (std::vector<std::vector<cell> > &matrix, const char* query, const char* target, int match, int mismatch, int gap, AlignmentType type){
             int i, j, k;
             int costs[3];
+
+	    int max = (type == AlignmentType::local) ? 0 : matrix[0][strlen(target)].cost;
+
             for (i=1; i <= strlen(query); i++) {
                 for (j=1; j <= strlen(target); j++) {
                     costs[MATCH] = matrix[i-1][j-1].cost + (target[j-1]==query[i-1] ? match : mismatch);
                     costs[INSERT] = matrix[i][j-1].cost + gap; 
                     costs[DELETE] = matrix[i-1][j].cost + gap;
+
                     matrix[i][j].cost = costs[MATCH];
                     matrix[i][j].parent = MATCH;
                     for (k=1; k<=2; k++) {
-                        if (costs[k] < matrix[i][j].cost) {
+                        if (costs[k] > matrix[i][j].cost) {
                             matrix[i][j].cost = costs[k];
                             matrix[i][j].parent = k;
                         }
                     }
+
+		    if(type == AlignmentType::local) {
+			if(matrix[i][j].cost < 0) {
+			    matrix[i][j].cost = 0;
+			 } else {
+			    max = std::max(max, matrix[i][j].cost);
+			 }
+		    } else if(type == AlignmentType::semi_global && j == strlen(target)) {
+			max = std::max(max, matrix[i][j].cost);
+		    }
                 }
             }
+
+	    return ((type == AlignmentType::local || type == AlignmentType::semi_global) ? max : matrix[strlen(query)][strlen(target)].cost);
         }
 
 
     }
-
-    enum class AlignmentType {global, local, semi_global};
 
     int pairwise_alignment(const char* query, unsigned int query_length,
                        const char* target, unsigned int target_length,
@@ -56,24 +91,11 @@ namespace orange {
                        int match,
                        int mismatch,
                        int gap) {
+
                         std::vector<std::vector<cell>> matrix (query_length+1, std::vector<cell>(target_length+1));
-                        switch(type) {
-                            case AlignmentType::global:
-                                initMatrix(matrix, query_length, target_length, gap);
-                                populateMatrix(matrix, query, target, match , mismatch, gap);
-                                return (matrix[query_length][target_length].cost);
-                            case AlignmentType::semi_global:
-                                
-                                //TO DO
 
-                                return 0;
-                            case AlignmentType::local:
-
-                                //TO DO
-
-                                return 0;
-                        }
-
+			initMatrix(matrix, query_length, target_length, gap, type);
+			return populateMatrix(matrix, query, target, match , mismatch, gap, type);
                        }
 
     int pairwise_alignment(const char* query, unsigned int query_length,
