b00c05db31892a5ba38f15d16c494d9a8e1819ed
Sanja Deur <sanja.deur@fer.hr>
2018-11-04 14:16:01 +0100

    Global alignment done and tested

---
+++ src/pink_alignment.cpp
@@ -0,0 +1,107 @@
+#include <cstring>
+#include <iostream>
+//#include "pink_alignment.hpp"
+
+namespace pink {
+    
+#define MATCH  0  
+#define INSERT 1  
+#define DELETE 2  
+#define HEAD   -1  
+    
+typedef struct {
+    int cost;  //cost of reaching this cell
+    int parent;  //parent cell
+} cell;
+
+void matrix_init(cell** matrix, int rows, int columns, int gap) {
+    for (int i = 0; i < rows; i++) {
+        matrix[i][0].cost = i * gap;
+        matrix[i][0].parent = HEAD;
+    }
+    
+    for (int j = 1; j < columns; j++) {
+        matrix[0][j].cost = j * gap;
+        matrix[0][j] .parent = HEAD;
+    }
+}
+
+int letter_match(char s, char t, int match, int mismatch) {
+    if (s == t) {
+        return match;
+        
+    } else {
+        return mismatch;
+    }
+}
+
+int string_compare(cell** matrix, const char *s, const char *t,
+                                    int match, int mismatch, int gap) {
+    unsigned i, j, k;  //counters
+    int options[3];  //cost of options
+
+    for (i = 1; i < strlen(s); i++) {
+        for (j = 1; j < strlen(t); j++) {
+            options[MATCH] = matrix[i-1][j-1].cost + letter_match(s[i], t[j], match, mismatch);
+            options[INSERT] = matrix[i][j-1].cost + gap;
+            options[DELETE] = matrix[i-1][j].cost + gap;
+             
+            matrix[i][j].cost = options[MATCH];
+            matrix[i][j].parent = MATCH;
+            
+            for (k = 1; k <= 2; k++) {
+                if (options[k] > matrix[i][j].cost) {
+                    matrix[i][j].cost = options[k];
+                    matrix[i][j].parent = k;
+                }
+            }
+        }
+    }
+    
+    std::cout << "\n~FINAL MATRIX~" << std::endl;
+    for (unsigned i = 0; i < strlen(s); i++) {
+        for (unsigned j = 0; j < strlen(t); j++) {
+            std::cout << matrix[i][j].cost << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    return matrix[strlen(s)-1][strlen(t)-1].cost;
+}
+
+int pairwise_alignment(const char* query, unsigned int query_length,
+                       const char* target, unsigned int target_length,
+                       //AlignmentType type,
+                       int match,
+                       int mismatch,
+                       int gap) {
+                           
+    cell** matrix = new cell*[query_length];  //dynamical matrix
+    for (unsigned i = 0; i < query_length; i++) {
+        matrix[i] = new cell[target_length];
+    }
+    
+    matrix_init(matrix, query_length, target_length, gap);
+    
+    std::cout << "\n~INITIAL MATRIX~" << std::endl;
+    for (unsigned i = 0; i < query_length; i++) {
+        for (unsigned j = 0; j < target_length; j++) {
+            std::cout << matrix[i][j].cost << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    return string_compare(matrix, query, target, match, mismatch, gap);
+}
+}
+
+int main() {
+    const char* query = "AAAC";
+    const char* target = "AGC";
+    
+    int cost = pink::pairwise_alignment(query, strlen(query), target, strlen(target), 0, 1, 1);
+                                
+    std::cout << "\nFinal cost: " << cost << std::endl;
+    return 0;
+}
+
