072e5eb7b9dcf0f42b65906b35a9e88f16352cae
lbcb-edu <josipa.lipovac@fer.hr>
2018-11-15 23:45:29 +0100

    commit #2

--- test/pink_mapper_test.cpp
+++ test/pink_mapper_test.cpp
@@ -3,11 +3,11 @@
 
 
 TEST (Pairwise_alignment, semi_global) {
-    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, semi_global, 1, -1, -1), 2);
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::semi_global, 1, -1, -1), 2);
 }
 TEST (Pairwise_alignment, global) {
-    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, global, 1, -1, -1), 0);
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::global, 1, -1, -1), 0);
 }
 TEST (Pairwise_alignment, local) {
-    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, local, 1, -1, -1), 2);
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::local, 1, -1, -1), 2);
 }
