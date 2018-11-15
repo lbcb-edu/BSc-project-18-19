13daf92916df9c5cee26d8806a4dcd84e9046e68
lbcb-edu <josipa.lipovac@fer.hr>
2018-11-15 23:03:32 +0100

    Test fixed

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,16 +1,18 @@
+#include "gtest/gtest.h"
 #include <src/pink_alignment.hpp>
 
+
 TEST (Pairwise_alignment, semi_global) {
-    EXPECT_EQ (2, pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, semi_global, 1, -1, -1));
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, semi_global, 1, -1, -1), 2);
 }
 TEST (Pairwise_alignment, global) {
-    EXPECT_EQ (0, pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, global, 1, -1, -1));
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, global, 1, -1, -1), 0);
 }
 TEST (Pairwise_alignment, local) {
-    EXPECT_EQ (2, pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, local, 1, -1, -1));
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, local, 1, -1, -1), 2);
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+//int main(int argc, char **argv) {
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
