de2b0ffa20a020215c8f3aa6f0beb489b07c27a0
Sanja Deur <sanja.deur@fer.hr>
2018-11-16 02:25:00 +0100

    Code enhancement

--- test/pink_mapper_test.cpp
+++ test/pink_mapper_test.cpp
@@ -1,13 +1,14 @@
 #include "gtest/gtest.h"
 #include <pink_alignment.hpp>
 
+TEST (Pairwise_alignment, global) {
+    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::global, 1, -1, -1), 0);
+}
 
 TEST (Pairwise_alignment, semi_global) {
     EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::semi_global, 1, -1, -1), 2);
 }
-TEST (Pairwise_alignment, global) {
-    EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::global, 1, -1, -1), 0);
-}
+
 TEST (Pairwise_alignment, local) {
     EXPECT_EQ (pink::pairwise_alignment ("TGCATAT", 7, "ATCCGAT", 7, pink::local, 1, -1, -1), 2);
 }
