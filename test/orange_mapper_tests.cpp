dca17d86b90d15bda4c78a9e72f8e576bd46e9e2
Luka Pozega <luka.pozega@fer.hr>
2018-11-16 12:21:09 +0100

    Add test for local alignment

--- test/orange_mapper_tests.cpp
+++ test/orange_mapper_tests.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 #include "orange_alignment.h"
 
-TEST (Pairwise_alignment, local) {
+TEST (Pairwise_alignment, global) {
     EXPECT_EQ (orange::pairwise_alignment ("GCATGCU", 7, "GATTACA", 7, orange::AlignmentType::global, 1, -1, -1), 0);
+}
+
+TEST (Pairwise_alignment, local) {
+    EXPECT_EQ (orange::pairwise_alignment ("ACCTAAGG", 8, "GGCTCAATCA", 10, orange::AlignmentType::local, 2, -1, -2), 6);
 }
