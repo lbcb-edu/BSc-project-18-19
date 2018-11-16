ebea4edb7fb9f4ec4421a5f993388e4e9b1b5449
RJPenic <rafael-josip.penic@fer.hr>
2018-11-16 16:14:54 +0100

    Added semi_global alignment test and target start position tests

--- test/orange_mapper_tests.cpp
+++ test/orange_mapper_tests.cpp
@@ -2,9 +2,26 @@
 #include "orange_alignment.h"
 
 TEST (Pairwise_alignment, global) {
-    EXPECT_EQ (orange::pairwise_alignment ("GCATGCU", 7, "GATTACA", 7, orange::AlignmentType::global, 1, -1, -1), 0);
+	
+	std::string cigar;
+	unsigned int target_start = 0;
+
+	EXPECT_EQ (orange::pairwise_alignment ("GCATGCU", 7, "GATTACA", 7, orange::AlignmentType::global, 1, -1, -1, cigar, target_start), 0);
+	EXPECT_EQ (target_start, 0);
 }
 
 TEST (Pairwise_alignment, local) {
-    EXPECT_EQ (orange::pairwise_alignment ("ACCTAAGG", 8, "GGCTCAATCA", 10, orange::AlignmentType::local, 2, -1, -2), 6);
+	std::string cigar;
+	unsigned int target_start = 0;
+
+	EXPECT_EQ (orange::pairwise_alignment ("ACCTAAGG", 8, "GGCTCAATCA", 10, orange::AlignmentType::local, 2, -1, -2, cigar, target_start), 6);
+	EXPECT_EQ (target_start, 2);
+}
+
+TEST (Pairwise_alignment, semi_global) {
+	std::string cigar;
+	unsigned int target_start = 0;
+
+	EXPECT_EQ (orange::pairwise_alignment ("ACCCAAGGG", 9, "GGCTCCATTA", 10, orange::AlignmentType::semi_global, 1, -1, -1, cigar, target_start), 3);
+	EXPECT_EQ (target_start, 1);
 }
