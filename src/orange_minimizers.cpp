ff004c6593a1cf18eb3068e6198ff828182d8e25
RJPenic <rafael-josip.penic@fer.hr>
2018-12-09 16:32:51 +0100

    Optimized minimizer algorithm and fixed errors in minimizers function

--- src/orange_minimizers.cpp
+++ src/orange_minimizers.cpp
@@ -7,20 +7,36 @@
 namespace orange {
 
 	std::map<char, char> complements_map = {
-		{A, T},
-		{T, A},
-		{G, C},
-		{C, G}
+		{'A', 'T'},
+		{'T', 'A'},
+		{'G', 'C'},
+		{'C', 'G'}
+	}
+
+	std::map<char, char> values_map = {
+		{'C', '0'},
+		{'A', '1'},
+		{'T', '2'},
+		{'G', '3'}
+	}
+
+	
+	std::string constructStringWithMap(std::string const base_string, std::map<char, char> const map) {
+		std:string result;
+
+		for(int i = 0; i < mer.length(); i++) {
+			result += map.at(base_string.at(i));
+		}
+
+		return result;
 	}
 
 	std::string constructComplementMer(std::string const mer) {
-		std:string complement;
+		return constructStringWithMap(mer, complements_map);
+	}
 
-		for(int i = 0; i < mer.length(); i++) {
-			complement += complements_map(mer.at(i));
-		}
-
-		return complement;
+	std::string constructValueString(std::string const mer) {
+		return constructStringWithMap(mer, values_map);
 	}
 
 	std::map<unsigned int, std::pair<std::string, bool>> minimizers(const char* sequence, unsigned int sequence_length, unsigned int k, unsigned int window_length) {
@@ -28,34 +44,26 @@ namespace orange {
 		seq.assign(sequence, sequence_length);
 		
 		std::map<unsigned int, std::pair<std::string, bool>> minimizers_map;
-		unsigned int position_of_last_found_min_mer = 0;
+		unsigned int position_of_last_found_min_mer = -1;
+		std::string last_min_mer;
 
 		for(unsigned int i = 0; window_length + k + i - 1 <= seq.length(); i++) {
-			std::string temp_seg = seq.substr(i, window_length + k + i - 1);
+			std::string temp_seg = seq.substr(i, window_length + k - 1);
 
-			std::string min_mer;
+			std::string min_mer = last_min_mer;
 			bool is_min_complement;
 			unsigned int min_pos = position_of_last_found_min_mer;
-			bool first_found = false;
+			bool first_found = (position_of_last_found_min_mer >= i && position_of_last_found_min_mer <= window_length + k + i - 1);
 
-			for(int j = position_of_last_found_min_mer - i; j + k <= temp_seg.length(); j++) {
-				if(j < 0) j = 0;
+			for(int j = first_found ? temp_seg.length() - k : 0; j + k <= temp_seg.length(); j++) {
 
-				std::string temp_mer_not_complement = seq.substr(j, k);
-				std::string temp_mer_complement = constructComplementMer(temp_mer);
+				std::string temp_mer_not_complement = temp_seg.substr(j, k);
+				std::string temp_mer_complement = constructComplementMer(temp_mer_not_complement);
 
-				std::string temp_mer;
-				bool is_complement;
+				bool is_complement = constructValueString(temp_mer_complement).compare(constructValueString(temp_mer_not_complement)) < 0;
+				std::string temp_mer = is_complement ? temp_mer_complement : temp_mer_not_complement;
 
-				if(temp_mer_complement.compare(temp_mer_not_complement) < 0) {
-					temp_mer = temp_mer_complement;
-					is_complement = true;
-				} else {
-					temp_mer = temp_mer_not_complement;
-					is_complement = false;
-				}
-
-				if(!first_found || min_mer.compare(temp_mer) > 0) {
+				if(!first_found || constructValueString(min_mer).compare(constructValueString(temp_mer)) > 0) {
 					if(!first_found) first_found = true;
 
 					min_mer = temp_mer;
@@ -67,6 +75,7 @@ namespace orange {
 			if(minimizers_map.count(min_pos) == 0) {
 				minimizers_map.insert(std::pair<unsigned int, std::pair<std::string, bool>>(min_pos, std::pair<std::string, bool>(min_mer, is_min_complement)));
 				position_of_last_found_min_mer = min_pos;
+				last_min_mer = min_mer;
 			}
 		}
 
