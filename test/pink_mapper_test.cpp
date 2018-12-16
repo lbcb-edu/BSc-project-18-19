6868670315aec17f52e16bde6b15e5f1d5321ce2
lbcb-edu <josipa.lipovac@fer.hr>
2018-12-16 23:35:08 +0100

    Changes with tests

--- test/pink_mapper_test.cpp
+++ test/pink_mapper_test.cpp
@@ -17,54 +17,25 @@ TEST (Pairwise_alignment, local) {
 TEST (Minimizers, TestAGTCCTTCTTCGTCT) {
     std::vector<std::tuple<unsigned int, unsigned int, bool>> minimizers = pink::minimizers("AGTCCTTCTTCGTCT", 15, 3, 3);
 
-    EXPECT_EQ(std::get<0>(minimizers.at(0)), 32);
-    EXPECT_EQ(std::get<1>(minimizers.at(0)), 2);
-    EXPECT_EQ(std::get<2>(minimizers.at(0)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(1)), 32);
-    EXPECT_EQ(std::get<1>(minimizers.at(1)), 2);
-    EXPECT_EQ(std::get<2>(minimizers.at(1)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(2)), 32);
-    EXPECT_EQ(std::get<1>(minimizers.at(2)), 2);
-    EXPECT_EQ(std::get<2>(minimizers.at(2)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(3)), 32);
-    EXPECT_EQ(std::get<1>(minimizers.at(3)), 3);
-    EXPECT_EQ(std::get<2>(minimizers.at(3)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(4)), 101);
-    EXPECT_EQ(std::get<1>(minimizers.at(4)), 6);
-    EXPECT_EQ(std::get<2>(minimizers.at(4)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(5)), 12);
-    EXPECT_EQ(std::get<1>(minimizers.at(5)), 7);
-    EXPECT_EQ(std::get<2>(minimizers.at(5)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(6)), 12);
-    EXPECT_EQ(std::get<1>(minimizers.at(6)), 7);
-    EXPECT_EQ(std::get<2>(minimizers.at(6)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(7)), 1);
-    EXPECT_EQ(std::get<1>(minimizers.at(7)), 9);
-    EXPECT_EQ(std::get<2>(minimizers.at(7)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(8)), 1);
-    EXPECT_EQ(std::get<1>(minimizers.at(8)), 9);
-    EXPECT_EQ(std::get<2>(minimizers.at(8)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(9)), 1);
-    EXPECT_EQ(std::get<1>(minimizers.at(9)), 9);
-    EXPECT_EQ(std::get<2>(minimizers.at(9)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(10)), 101);
-    EXPECT_EQ(std::get<1>(minimizers.at(10)), 12);
-    EXPECT_EQ(std::get<2>(minimizers.at(10)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(11)), 231);
-    EXPECT_EQ(std::get<1>(minimizers.at(11)), 0);
-    EXPECT_EQ(std::get<2>(minimizers.at(11)), 0);
-}
-
-TEST(MapperTest, TestCGACT){
-    std::vector<std::tuple<unsigned int, unsigned int, bool>> minimizers = pink::minimizers("CGACT", 5, 3, 1);
-    
-    EXPECT_EQ(std::get<0>(minimizers.at(0)), 100);
+    EXPECT_EQ(std::get<0>(minimizers.at(0)), 33);
     EXPECT_EQ(std::get<1>(minimizers.at(0)), 0);
     EXPECT_EQ(std::get<2>(minimizers.at(0)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(1)), 310);
-    EXPECT_EQ(std::get<1>(minimizers.at(1)), 1);
-    EXPECT_EQ(std::get<2>(minimizers.at(1)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(2)), 201);
+    EXPECT_EQ(std::get<0>(minimizers.at(1)), 14);
+    EXPECT_EQ(std::get<1>(minimizers.at(1)), 3);
+    EXPECT_EQ(std::get<2>(minimizers.at(1)), 0);
+    EXPECT_EQ(std::get<0>(minimizers.at(2)), 14);
     EXPECT_EQ(std::get<1>(minimizers.at(2)), 2);
-    EXPECT_EQ(std::get<2>(minimizers.at(2)), 0);
+    EXPECT_EQ(std::get<2>(minimizers.at(2)), 1);
+    EXPECT_EQ(std::get<0>(minimizers.at(3)), 17);
+    EXPECT_EQ(std::get<1>(minimizers.at(3)), 6);
+    EXPECT_EQ(std::get<2>(minimizers.at(3)), 0);
+    EXPECT_EQ(std::get<0>(minimizers.at(4)), 1);
+    EXPECT_EQ(std::get<1>(minimizers.at(4)), 9);
+    EXPECT_EQ(std::get<2>(minimizers.at(4)), 1);
+    EXPECT_EQ(std::get<0>(minimizers.at(5)), 6);
+    EXPECT_EQ(std::get<1>(minimizers.at(5)), 7);
+    EXPECT_EQ(std::get<2>(minimizers.at(5)), 0);
+    EXPECT_EQ(std::get<0>(minimizers.at(6)), 17);
+    EXPECT_EQ(std::get<1>(minimizers.at(6)), 12);
+    EXPECT_EQ(std::get<2>(minimizers.at(6)), 0);
 }
