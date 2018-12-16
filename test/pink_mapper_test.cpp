f966e342e2c91922f6841618f323011d880dce2e
lbcb-edu <josipa.lipovac@fer.hr>
2018-12-16 23:56:16 +0100

    Fixed minimizers test2

--- test/pink_mapper_test.cpp
+++ test/pink_mapper_test.cpp
@@ -17,25 +17,25 @@ TEST (Pairwise_alignment, local) {
 TEST (Minimizers, TestTGACGTACATGGACA) {
     std::vector<std::tuple<unsigned int, unsigned int, bool>> minimizers = pink::minimizers("TGACGTACATGGACA", 15, 3, 3);
 
-    EXPECT_EQ(std::get<0>(minimizers.at(0)), 33);
-    EXPECT_EQ(std::get<1>(minimizers.at(0)), 0);
+    EXPECT_EQ(std::get<0>(minimizers.at(0)), 14);
+    EXPECT_EQ(std::get<1>(minimizers.at(0)), 2);
     EXPECT_EQ(std::get<2>(minimizers.at(0)), 1);
     EXPECT_EQ(std::get<0>(minimizers.at(1)), 14);
     EXPECT_EQ(std::get<1>(minimizers.at(1)), 3);
     EXPECT_EQ(std::get<2>(minimizers.at(1)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(2)), 14);
-    EXPECT_EQ(std::get<1>(minimizers.at(2)), 2);
-    EXPECT_EQ(std::get<2>(minimizers.at(2)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(3)), 17);
-    EXPECT_EQ(std::get<1>(minimizers.at(3)), 6);
+    EXPECT_EQ(std::get<0>(minimizers.at(2)), 17);
+    EXPECT_EQ(std::get<1>(minimizers.at(2)), 6);
+    EXPECT_EQ(std::get<2>(minimizers.at(2)), 0);
+    EXPECT_EQ(std::get<0>(minimizers.at(3)), 6);
+    EXPECT_EQ(std::get<1>(minimizers.at(3)), 7);
     EXPECT_EQ(std::get<2>(minimizers.at(3)), 0);
     EXPECT_EQ(std::get<0>(minimizers.at(4)), 1);
     EXPECT_EQ(std::get<1>(minimizers.at(4)), 9);
     EXPECT_EQ(std::get<2>(minimizers.at(4)), 1);
-    EXPECT_EQ(std::get<0>(minimizers.at(5)), 6);
-    EXPECT_EQ(std::get<1>(minimizers.at(5)), 7);
+    EXPECT_EQ(std::get<0>(minimizers.at(5)), 17);
+    EXPECT_EQ(std::get<1>(minimizers.at(5)), 12);
     EXPECT_EQ(std::get<2>(minimizers.at(5)), 0);
-    EXPECT_EQ(std::get<0>(minimizers.at(6)), 17);
-    EXPECT_EQ(std::get<1>(minimizers.at(6)), 12);
-    EXPECT_EQ(std::get<2>(minimizers.at(6)), 0);
+    EXPECT_EQ(std::get<0>(minimizers.at(6)), 33);
+    EXPECT_EQ(std::get<1>(minimizers.at(6)), 0);
+    EXPECT_EQ(std::get<2>(minimizers.at(6)), 1);
 }
