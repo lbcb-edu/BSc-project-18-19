44f5b2aaedd9e192310e64406e7701efa228c81d
RJPenic <rafael-josip.penic@fer.hr>
2019-01-10 00:10:10 +0100

    Added reference index creation, merged FASTAEntity and FASTQEntity into one class, set up mapper functionality

--- src/orange_mapper.cpp
+++ src/orange_mapper.cpp
@@ -16,6 +16,8 @@
 #include "orange_alignment.h"
 #include "orange_minimizers.h"
 
+#define DEFAULT_BAND_OF_WIDTH 500
+
 std::vector<std::string> FASTAExtensionVector{".fasta", ".fa", ".fasta.gz", ".fa.gz"};
 std::vector<std::string> FASTQExtensionVector{".fastq", ".fq", ".fastq.gz", ".fq.gz"};
 
@@ -42,6 +44,10 @@ struct alignmentStruct {
 };
 typedef struct alignmentStruct alignment;
 
+auto cmp_def = [](std::pair<unsigned int,unsigned int> const & a, std::pair<unsigned int,unsigned int> const & b) { 
+ 		return a.second != b.second?  a.second > b.second : a.first > b.first;
+	};
+
 struct statsStruct {
 	uint32_t max;
 	uint32_t min;
@@ -59,13 +65,14 @@ struct statsStruct {
 };
 typedef struct statsStruct stats;
 
-class FASTAEntity {
+class FASTAQEntity {
 	
 	public:
 		std::string name;
 		std::string sequence;
+		std::string quality;
 		
-		FASTAEntity(
+		FASTAQEntity(
 			const char *name, uint32_t name_length,
 			const char *sequence, uint32_t sequence_length) {
 
@@ -73,21 +80,13 @@ class FASTAEntity {
 
 			(this->sequence).assign(sequence, sequence_length);
 		}
-};
 
-class FASTQEntity : public FASTAEntity {
-
-	public:
-		std::string quality;
-		
-		FASTQEntity(
+		FASTAQEntity(
 			const char* name, uint32_t name_length,
 			const char* sequence, uint32_t sequence_length,
-			const char* quality, uint32_t quality_length)
-			: FASTAEntity(name, name_length, sequence, sequence_length) {
+			const char* quality, uint32_t quality_length) : FASTAQEntity(name, name_length, sequence, sequence_length) {
 			
 			(this->quality).assign(quality, quality_length);
-
 		}
 };
 
@@ -144,11 +143,11 @@ void printStatistics(stats const &fileStats, std::string const &filePath) {
 	fprintf(stderr, "------------------\n");
 }
 
-template<class T>
-void calculateStats(std::vector<std::unique_ptr<T>> const &entities, stats *fileStats, alignment alignment) {
+void calculateStats(std::vector<std::unique_ptr<FASTAQEntity>> const &entities, stats *fileStats, alignment alignment) {
 	
 	for(auto const& p : entities) {
 		fileStats->num_of_seq++;
+
 		fileStats->total_length += (p-> sequence).length();
 
 		fileStats->max = (fileStats->max > (p-> sequence).length()) ? fileStats->max : (p-> sequence).length();
@@ -185,60 +184,68 @@ void calculateStats(std::vector<std::unique_ptr<T>> const &entities, stats *file
 
 }
 
-void readFASTQFile(std::string const &filePath, alignment alignment) {
-	std::vector<std::unique_ptr<FASTQEntity>> fastq_objects;
-	auto fastq_parser = bioparser::createParser<bioparser::FastqParser, FASTQEntity>(filePath);
+std::vector<std::unique_ptr<FASTAQEntity>> readFASTQFile(std::string const &filePath, alignment alignment, bool calc_statistics) {
+	std::vector<std::unique_ptr<FASTAQEntity>> fastq_objects;
+	auto fastq_parser = bioparser::createParser<bioparser::FastqParser, FASTAQEntity>(filePath);
 
 	uint64_t size_in_bytes = 500 * 1024 * 1024; // 500 MB
-	stats fileStats;
 	while (true) {
 		auto status = fastq_parser->parse_objects(fastq_objects, size_in_bytes);
 		if (status == false) {
 			break;
 		}
 	}
-	calculateStats<FASTQEntity>(fastq_objects, &fileStats, alignment);	
-	printStatistics(fileStats, filePath);
+	
+	if(calc_statistics) {
+		stats fileStats;
+		calculateStats(fastq_objects, &fileStats, alignment);	
+		printStatistics(fileStats, filePath);
+	}
+
+	return fastq_objects;
 }
 
-void readFASTAFile(std::string const &filePath, alignment alignment) {
-	std::vector<std::unique_ptr<FASTAEntity>> fasta_objects;
-	auto fasta_parser = bioparser::createParser<bioparser::FastaParser, FASTAEntity>(filePath);
+std::vector<std::unique_ptr<FASTAQEntity>> readFASTAFile(std::string const &filePath, alignment alignment, bool calc_statistics) {
+	std::vector<std::unique_ptr<FASTAQEntity>> fasta_objects;
+	auto fasta_parser = bioparser::createParser<bioparser::FastaParser, FASTAQEntity>(filePath);
 	
 	fasta_parser->parse_objects(fasta_objects, -1);
 	
-	stats fileStats;
-	calculateStats<FASTAEntity>(fasta_objects, &fileStats, alignment);
+	if(calc_statistics) {
+		stats fileStats;
+		calculateStats(fasta_objects, &fileStats, alignment);
+		printStatistics(fileStats, filePath);
+	}
 
-	printStatistics(fileStats, filePath);
+	return fasta_objects;
 }
 
 void calculateAndPrintOutStatistics(std::string const &firstFilePath, std::string const &secondFilePath, bool isFirstFileFASTA, alignment alignment) {
 	if(isFirstFileFASTA) {
-		readFASTAFile(firstFilePath, alignment);
+		readFASTAFile(firstFilePath, alignment, true);
 	} else {
-		readFASTQFile(firstFilePath, alignment);
+		readFASTQFile(firstFilePath, alignment, true);
 	}
 	alignment.type = orange::AlignmentType::no_alignment;
-	readFASTAFile(secondFilePath, alignment);
+	readFASTAFile(secondFilePath, alignment, true);
 }
 
-void createCSVfile(std::string const &filePath, int k, int window_lenght, float f) {
-	std::vector<std::unique_ptr<FASTAEntity>> fasta_objects;
-	auto fasta_parser = bioparser::createParser<bioparser::FastaParser, FASTAEntity>(filePath);
-	
-	fasta_parser->parse_objects(fasta_objects, -1);
+void findMinimizers(std::string const &filePath, int k, int window_lenght, double f, bool isFirstFASTA) {
+	alignment al;
+	std::vector<std::unique_ptr<FASTAQEntity>> fastaq_objects = 
+		isFirstFASTA ? readFASTAFile(filePath, al, false) : readFASTQFile(filePath, al, false);
 
 	std::vector<std::tuple<unsigned int, unsigned int, bool>> minimizers;
 	std::unordered_map<unsigned int, unsigned int> csvMap;
 
-	unsigned int end=fasta_objects.size();
+	unsigned int end=fastaq_objects.size();
 	unsigned int counter = 0;
 
 	std::cout << "Looking for minimizers, please wait\n";
 
-	for (auto const &x : fasta_objects) {
+	for (auto const &x : fastaq_objects) {
 		minimizers = orange::minimizers(x->sequence.c_str(), x->sequence.length(), k, window_lenght);
+
 		++counter;
 
 		std::cout << "\r" << "Progress: " << counter << "/" << end << std::flush;
@@ -250,28 +257,99 @@ void createCSVfile(std::string const &filePath, int k, int window_lenght, float 
 
 	printf("\nNumber of minimizers: %lu\n", csvMap.size());
 
-	auto cmp = [](std::pair<unsigned int,unsigned int> const & a, std::pair<unsigned int,unsigned int> const & b) { 
-     	return a.second != b.second?  a.second > b.second : a.first > b.first;
-	};
-
 	std::vector<std::pair<unsigned int, unsigned int>> vec(csvMap.begin(), csvMap.end());
 
-	std::sort(vec.begin(), vec.end(), cmp);
+	std::sort(vec.begin(), vec.end(), cmp_def);
 
 	int pos = f*csvMap.size();
 
-	printf("%d", vec[pos].first);
+	printf("%d", vec[pos].second);
 	
 	std::cout << "\nDone!\n";
-	
+}
+
+std::unordered_map<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool>>> constructMinimizerIndex(double f, int k, int window_lenght, std::unique_ptr<FASTAQEntity> const &reference_gen) {
+	printf("Looking for reference minimizers...\n");
+	std::vector<std::tuple<unsigned int, unsigned int, bool>> ref_minimizers = orange::minimizers(reference_gen->sequence.c_str(), reference_gen->sequence.length(), k, window_lenght);
+	printf("Reference minimizers found!\n");
+
+	printf("Creating reference index...\n");
+	std::unordered_map<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool>>> ref_index;
+
+	for(auto const &t : ref_minimizers) {
+		ref_index[std::get<0>(t)].emplace_back(t);
+	}
+	printf("Reference index created!\n");
+
+	printf("Removing too frequent minimizers from reference index...\n");
+	auto cmp = [](std::pair<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool>>> const & a, std::pair<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool>>> const & b) { 
+ 		return a.second.size() > b.second.size();
+	};
+	std::vector<std::pair<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool>>>> temp_vec(ref_index.begin(), ref_index.end());
+
+	std::sort(temp_vec.begin(), temp_vec.end(), cmp);
+
+	unsigned int border = f * temp_vec.size();
+	for(unsigned int i = 0; i < border; i++) {
+		ref_index.erase(temp_vec[i].first);
+	}
+	printf("Too frequent reference minimizers removed!\n");
+
+	return ref_index;
+}
+
+void constructAndPrintPAF(std::string const &firstFilePath, std::string const &secondFilePath, bool isFirstFASTA, int k, int window_lenght, double f, bool cigar) {
+	alignment al;
+	std::vector<std::unique_ptr<FASTAQEntity>> fastaq_objects = 
+		isFirstFASTA ? readFASTAFile(firstFilePath, al, false) : readFASTQFile(firstFilePath, al, false);
+
+	std::vector<std::unique_ptr<FASTAQEntity>> reference_gen_vec = readFASTAFile(secondFilePath, al, false);
+	for(auto const &reference_gen : reference_gen_vec) {
+		std::unordered_map<unsigned int, std::vector<std::tuple<unsigned int, unsigned int, bool>>> ref_index = constructMinimizerIndex(f, k, window_lenght, reference_gen);
+
+		for(auto const &x : fastaq_objects) {
+			std::vector<std::tuple<unsigned int, unsigned int, bool>> fragment_minimizers = orange::minimizers(reference_gen->sequence.c_str(), reference_gen->sequence.length(), k,window_lenght);
+			std::vector<std::tuple<unsigned int, short, long int, unsigned int>> vec;	
+
+			for(auto const &t : fragment_minimizers) {
+				for(auto const &rt : ref_index[std::get<0>(t)]) {
+					if(std::get<2>(t) == std::get<2>(rt)) {
+						vec.emplace_back(std::get<0>(rt), 0, std::get<1>(t) - std::get<1>(rt), std::get<1>(rt));
+					} else {
+						vec.emplace_back(std::get<0>(rt), 1, std::get<1>(t) + std::get<1>(rt), std::get<1>(rt));
+					}
+				}
+			}
+			
+			auto custom_cmp = [](std::tuple<unsigned int, short, long int, unsigned int> const & a, std::tuple<unsigned int, short, long int, unsigned int> const & b) { 
+				if(std::get<0>(a) != std::get<0>(b)) return std::get<0>(a) < std::get<0>(b);		
+				if(std::get<1>(a) != std::get<1>(b)) return std::get<1>(a) < std::get<1>(b);
+				if(std::get<2>(a) != std::get<2>(b)) return std::get<2>(a) < std::get<2>(b);
+				return std::get<3>(a) < std::get<3>(b);
+			};// ???
+
+			std::sort(vec.begin(), vec.end(), custom_cmp);
+
+			unsigned int b = 0;
+			for(unsigned int i = 0; i < vec.size(); ++i) {
+				if(i == vec.size() - 1 ||
+					std::get<0>(vec[i + 1]) != std::get<0>(vec[i]) ||
+					std::get<1>(vec[i + 1]) != std::get<1>(vec[i]) ||
+					std::get<2>(vec[i + 1]) - std::get<2>(vec[i]) >= DEFAULT_BAND_OF_WIDTH) {
+					//longest linear chain here <==
+					b = i + 1;
+				}
+			}
+		}
+	}
 }
 
 int main(int argc, char** argv) {
 
 	srand((unsigned)time(0)); 
 	alignment alignment;
-	int k=15, window_lenght=5;
-	float f = 0.001;
+	int k=15, window_lenght= 5;
+	double f = 0.001;
 
 	char optchr;
 
@@ -341,7 +419,9 @@ int main(int argc, char** argv) {
 
 	calculateAndPrintOutStatistics(firstFilePath, secondFilePath, isFirstFASTA, alignment);
 
-	createCSVfile(firstFilePath, k, window_lenght, f);
+//	findMinimizers(firstFilePath, k, window_lenght, f, isFirstFASTA);
+
+	constructAndPrintPAF(firstFilePath, secondFilePath, isFirstFASTA, k, window_lenght, f, false);
 
 	return 0;
 }
